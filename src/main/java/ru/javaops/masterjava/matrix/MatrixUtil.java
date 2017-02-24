83c4c252c404193729f8683411223ba75ba99805
Sergey Klimtsev <grayfox666893@gmail.com>
2017-02-24 15:31:32 +0500

    hw1 complete

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,11 @@
 package ru.javaops.masterjava.matrix;
 
+import ru.javaops.masterjava.service.MailService;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 
 /**
  * gkislin
@@ -11,23 +14,64 @@ import java.util.concurrent.ExecutorService;
 public class MatrixUtil {
 
     // TODO implement parallel multiplication matrixA*matrixB
+    @SuppressWarnings("Duplicates")
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+        final CompletionService<Boolean> completionService = new ExecutorCompletionService<>(executor);
+
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+
+
+        List<Future<Boolean>> futureList = new ArrayList<>();
+
+        for (int j = 0; j < matrixSize; j++) {
+            int finalJ = j;
+            Future<Boolean> future = completionService.submit(() -> {
+                int[] thatColumn = new int[matrixSize];
+                for (int k = 0; k < matrixSize; k++) {
+                    thatColumn[k] = matrixB[k][finalJ];
+                }
+                for (int i = 0; i < matrixSize; i++) {
+                    int[] thisRow = matrixA[i];
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += thisRow[k] * thatColumn[k];
+                    }
+                    matrixC[i][finalJ] = sum;
+                }
+            }, true);
+            futureList.add(future);
+
+        }
+        while (!futureList.isEmpty()) {
+            Future<Boolean> future = completionService.poll();
+            if (future != null) {
+                futureList.remove(future);
+            }
+        }
         return matrixC;
     }
 
+
     // TODO optimize by https://habrahabr.ru/post/114797/
+    @SuppressWarnings("Duplicates")
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
+        int[] thatColumn = new int[matrixSize];
+
+
+        for (int j = 0; j < matrixSize; j++) {
+            for (int k = 0; k < matrixSize; k++) {
+                thatColumn[k] = matrixB[k][j];
+            }
+            for (int i = 0; i < matrixSize; i++) {
+                int[] thisRow = matrixA[i];
                 int sum = 0;
                 for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+                    sum += thisRow[k] * thatColumn[k];
                 }
                 matrixC[i][j] = sum;
             }
